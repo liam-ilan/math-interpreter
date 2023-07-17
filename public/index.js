42ad8ec8da95bb5d2bcd10e675a28fd050a454b8
Liam Ilan <snowboardsheep@gmail.com>
2023-07-17 15:33:19 -0700

    Rudementary css.

--- public/index.js
+++ public/index.js
@@ -6,12 +6,42 @@ const outEls = {
   execute: document.getElementById("execute")
 }
 
+// elements for selecting viewing mode
+const selectionEls = {
+  code: document.getElementById("codeSelect"),
+  tokens: document.getElementById("tokensSelect"),
+  ast: document.getElementById("astSelect"),
+  execute: document.getElementById("executeSelect")
+}
+
 // elements for input
 const exprEl = document.getElementById("expr")
 const submitEl = document.getElementById("submit")
 
 // output state (tokens, ast, execute, or code)
 let outState = "code"
+
+// updates ui
+function updateView(viewState) {
+  // reset buttons and view panes
+  Object.keys(selectionEls).forEach((key) => {
+    selectionEls[key].classList.remove("selected")
+    outEls[key].classList.remove("show")
+  })
+  
+  // add selected class
+  selectionEls[viewState].classList.add("selected")
+  outEls[viewState].classList.add("show")
+}
+
+updateView("execute")
+
+// add event listenrs
+Object.keys(selectionEls).forEach((key) => {
+  selectionEls[key].addEventListener("click", (e) => {
+    updateView(key)
+  })
+})
 
 // wasm module
 Module = {
@@ -20,8 +50,7 @@ Module = {
     else if (text == "TOKENS") outState = "tokens"
     else if (text == "AST") outState = "ast"
     else if (text == "EXECUTE") outState = "execute"
-
-    outEls[outState].innerHTML += text + "</br>"
+    else outEls[outState].innerHTML += text + "</br>"
   },
 
   onRuntimeInitialized: () => {
@@ -29,11 +58,14 @@ Module = {
     // wrap C method
     let runCode = Module.cwrap("run", null, ["string", "number"])
 
-    // on click, run
-    submitEl.addEventListener("click", (e) => {
+    // submit method
+    function submitMethod(e) {
       outState = "code"
       Object.values(outEls).forEach((item) => item.innerHTML = "")
       runCode(exprEl.value, exprEl.value.length)
-    })
+    }
+    // on click or enter, run
+    submitEl.addEventListener("click", submitMethod)
+    exprEl.addEventListener("keydown", (e) => e.key === "Enter" ? submitMethod() : null)
   }
 }
